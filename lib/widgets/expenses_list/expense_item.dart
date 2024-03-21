6c7c9639bf902582ff596922d7cf46a6fa9fa24a
J.Turowski <turek.priv@gmail.com>
2024-03-21 11:03:51 +0100

    minor fixes

---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,13 @@
+import 'package:expense_tracker/models/expense_model.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final ExpenseModel expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(child: Text(expense.title),);
+  }
+}
